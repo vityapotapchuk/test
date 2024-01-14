2fc68e30aecc6d47f6f433aa52dc262f37329725
vitosigm13611 <smplchnl@gmail.com>
2024-01-13 22:26:51 -0800

    to do task object

--- script.js
+++ script.js
@@ -1,5 +1,45 @@
-const myNumber = 42
+// #1
+// let obj = {
+//     width: 200,
+//     height: 300,
+//     title: "My menu"
+//   };
+  
+ 
+//   function multiplyNumeric(obj) {
+//     for (let key in obj) {
+//         if (typeof obj[key] === 'number'){
+//             obj[key] *= 2;
+//         }
+//     }
+//   }
 
-console.log(localStorage.getItem('number', myNumber.toString()));
-localStorage.setItem('number', myNumber);
-console.log(localStorage.getItem('number', myNumber.toString()));
+// multiplyNumeric(obj);
+// console.log(obj);
+
+
+//#2
+// let user = {
+//     name: "John",
+//     age: 30,
+// }
+
+// let clone = {};
+
+// for (let key in user) {
+//     clone[key] = user[key];
+// }
+// console.log(clone);
+
+//#3
+// let person = {
+//     name: 'John',
+//     cars: {
+//         lambo: 'Urus',
+//         toyota: 'Supra',
+// }
+
+// }
+// console.log (person.cars.toyota);
+
+//#4
